7ab942b456d430454ebdb4c14747bfde6f696b11
Silver-02 <chlehfcp02@gmail.com>
2024-08-01 19:22:17 +0900

    Level-04 작성

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
+     for(;;) {
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
@@ -39,5 +39,14 @@ public class App {
                 break;
         }
         System.out.println("결과: " + result);
+
+        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+
+        if (sc.next().equals("exit")) {
+            break;
+        } else {
+            continue;
+        }
+     }
     }
 }
