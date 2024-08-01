410b954c4071a18b2922c87f3507778f990ac86e
Silver-02 <chlehfcp02@gmail.com>
2024-08-01 19:11:50 +0900

    Level-03 작성

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -12,9 +12,32 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
-        System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
+        System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
         char operator = sc.next().charAt(0);
 
-        System.out.println("operator = " + operator);
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("잘못 입력하셨습니다.");
+                break;
+        }
+        System.out.println("결과: " + result);
     }
 }
